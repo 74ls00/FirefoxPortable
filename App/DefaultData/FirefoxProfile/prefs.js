f9a559cfde3cbd755a218a8a707159a44c14edf0
74ls00 <userled@yandex.ru>
2018-11-22 01:04:52 +0300

    green

--- App/DefaultData/FirefoxProfile/prefs.js
+++ App/DefaultData/FirefoxProfile/prefs.js
@@ -10,6 +10,11 @@
  */
    
 /*Browser*/
+
+/*браузер*/
+/*user_pref("general.useragent.overridepreference", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586");*/
+
+
 /*!DEBUG*/
 user_pref("view_source.editor.external", true);
 user_pref("view_source.editor.path", "G:\\Programs\\portableapps.com\\PortableApps\\Notepad++Portable\\Notepad++Portable.exe");
